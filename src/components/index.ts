ef521320129daccef1b6c8aa0e93c815b9ade84c
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2024-07-21 07:37:26 +0300

    added index for imports  components

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export * from "./ui/avatar"
+export * from "./ui/badge"
+export * from "./ui/button"
+export * from "./ui/card"
+export { default as BlogCard } from './BlogCard.vue'
+export { default as TagChip } from './TagChip.vue'
+export { default as NavBar } from './NavBar.vue'
+export { default as FormInputField } from './createNewBlog/FormField.vue'
