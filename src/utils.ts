6b7de131f94c7a545f27ed09578a7dd01ac0ce78
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2024-07-21 07:37:09 +0300

    added date utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from 'clsx'
+import dayjs from 'dayjs'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
@@ -9,4 +10,11 @@ export function cn(...inputs: ClassValue[]) {
 export function getInitials(name: string) {
   const [firstName, lastName] = name.split(" ")
   return firstName[0] + " " + lastName[0]
+}
+
+export function formatDisplayDate(date: string) {
+  return dayjs(date).format("D-MMM YY")
+}
+export function formatStoreDate(date: Date) {
+  return date.toISOString()
 }
