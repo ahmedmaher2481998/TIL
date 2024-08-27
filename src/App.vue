b606e7c5ba8a2df1b14384d69d466cc7e23b231f
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-08-28 02:20:14 +0300

    fix: suspense is not needed

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,10 @@ import { NavBar, Toaster, PopUp } from '@/components'
   <header>
     <NavBar />
   </header>
-  <Suspense>
-    <template #fallback>
-      <div>Loading...</div>
-    </template>
-    <RouterView />
-  </Suspense>
+  <RouterView />
 
   <footer
-    class="w-full my-auto bg-primary text-primary-foreground items-center justify-center flex h-24 p-6"
+    class="w-full my-auto bg-foreground text-primary-foreground items-center justify-center flex h-24 p-6"
   >
     all rights reserved Ahmed maher 2024&copy;
   </footer>
