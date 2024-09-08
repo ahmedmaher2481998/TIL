4983b6d424b8ffda805c38558ea443e45c401690
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-09-08 11:24:05 +0300

    add: loader indicator

---
+++ src/components/shared/Loader-component.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { cn } from '@/utils.ts'
+import { type ClassValue } from 'clsx'
+import { LoaderCircle } from 'lucide-vue-next';
+
+const { classes = undefined } = defineProps<{
+  classes: ClassValue[] | undefined
+}>()
+
+</script>
+<template>
+  <LoaderCircle :class="cn('animate-spin', classes)" />
+</template>
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,4 @@
 export { default as NavBar } from './NavBar.vue'
 export { default as PopUp } from './PopUp.vue'
 export { default as ImageUpload } from './ImageUpload.vue'
+export { default as Loader } from './Loader-component.vue'
