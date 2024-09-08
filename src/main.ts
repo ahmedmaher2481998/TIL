c21fa33b46846dcbbcfcbf3d68029902fe399bb5
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-09-08 11:23:56 +0300

    add: tan-stack query

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import '../styles.css'
 import App from './App.vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+
+
 // Supports weights 100-900
 import '@fontsource-variable/noto-sans'
 import router from '@/router'
 
 const app = createApp(App)
-
 const pinia = createPinia()
 
-app.use(pinia).use(router).mount('#app')
+app
+  .use(pinia)
+  .use(router)
+  .use(VueQueryPlugin)
+  .mount('#app')
