3311d964a3bb47fb4998df552ca85554d3f9f4b2
Ahmed maher <ahmedmaher2481998@gmail.com>
2024-09-09 10:03:11 +0300

    new: view all blogs route

--- src/views/DisplayBlogsWithTag.view.vue
+++ src/views/DisplayBlogsWithTag.view.vue
@@ -1,51 +1,60 @@
 <script setup lang="ts">
-import { BlogCard } from '@/components'
+import { BlogCard, Skeleton } from '@/components'
+import { useTags } from '@/stores'
 import supabase from '@/supabase'
 import { Tables } from '@/types'
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
-
+import { notify } from '@/utils'
+import { onBeforeMount, onMounted, ref, Suspense } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+const { getBlogsByTagSlug } = useTags()
 const route = useRoute()
-const tagSlug = route.params.tagSlug
-const selectAllBlogsQuery = `
-blogs(
-id, title, slug, description, tldr, image_url, read_count, created_at, updated_at,
-${Tables.Tags} (*),
-profiles(user_metadata,email,id)
-)
-`
-const { data } = await supabase
-  .from(Tables.Tags)
-  .select(selectAllBlogsQuery)
-  .eq('slug', route.params.tagSlug)
-  .single()
-console.log('blog data: ', data)
+const tagSlug = route.params.tagSlug as string
+const router = useRouter()
+let data = ref<Awaited<ReturnType<typeof getBlogsByTagSlug>>>()
+onMounted(async () => {
+  try {
+    data.value = await getBlogsByTagSlug(tagSlug)
+  } catch (error) {
+    notify.error({
+      title: 'Sorry this page is not available',
+      description: 'Failed to retrieve blogs by tag slug.',
+    })
+    router.back()
+  }
+})
+
+
 </script>
 <template>
   <main class="w-full min-h-screen">
-    <div>displaying blog containing tag with slug : {{ tagSlug }}</div>
-    <!-- <div v-if="" class="container pb-20 grid gap-4 grid-cols-2 lg:grid-cols-3">
-    <div v-for="i in 5 + Math.floor(Math.random() * 10)" :key="i" class="flex flex-col space-y-3">
-      <Skeleton class="h-[125px] w-[250px] rounded-xl" />
-      <div class="space-y-2">
-        <Skeleton class="h-4 w-[250px]" />
-        <Skeleton class="h-4 w-[200px]" />
-      </div>
-    </div>
-  </div> -->
+
     <div class="container pb-20 grid gap-4 grid-cols-2 lg:grid-cols-3">
-      <BlogCard v-for="blog in data?.blogs" :blog="{
-        id: blog.id,
-        title: blog.title,
-        slug: blog.slug,
-        tldr: blog.tldr,
-        image: blog.image_url,
-        avatar: blog.profiles?.user_metadata?.avatar as string ?? '',
-        author: blog.profiles?.user_metadata?.name as string ?? '',
-        tags: blog.tags,
-        read_count: blog.read_count,
-        created_at: blog.created_at
-      }" :key="blog.id" />
+      <Suspense>
+        <BlogCard v-for="blog in data?.blogs" :blog="{
+          id: blog.id,
+          title: blog.title,
+          slug: blog.slug,
+          tldr: blog.tldr,
+          image: blog.image_url,
+          // @ts-ignore
+          avatar: blog.profiles?.user_metadata?.avatar as string ?? '',
+          // @ts-ignore
+          author: blog.profiles?.user_metadata?.name as string ?? '',
+          tags: blog.tags,
+          read_count: blog.read_count,
+          created_at: blog.created_at
+        }" :key="blog.id" />
+        <template #fallback>
+          <div v-for="i in 5 + Math.floor(Math.random() * 10)" :key="i" class="flex flex-col space-y-3">
+            <Skeleton class="h-[125px] w-[250px] rounded-xl" />
+            <div class="space-y-2">
+              <Skeleton class="h-4 w-[250px]" />
+              <Skeleton class="h-4 w-[200px]" />
+            </div>
+          </div>
+        </template>
+      </Suspense>
+
     </div>
   </main>
 </template>
